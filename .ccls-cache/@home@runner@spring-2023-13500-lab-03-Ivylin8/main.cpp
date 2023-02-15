7eca197bd14d8f29eff91fa8f16465e699c132e6
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-02-15 03:49:41 +0000

    task a

---
+++ .ccls-cache/@home@runner@spring-2023-13500-lab-03-Ivylin8/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include "reservoir.h"
+
+
+int main()
+{
+  std::cout<< "East Bsin Storage: " << 
+
+  
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-// add your code as appropriate 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include "reservoir.h"
+
 
 int main()
 {
+  std::cout<< "East Basin Storage: " << get_east_storage("01/01/2018") << "billion gallons"<<std::endl;
+   std::cout<< "East Basin Storage: " << get_east_storage("12/01/2018") << "billion gallons"<<std::endl;
   
 
+return 0;
   
-  return 0;
 }
