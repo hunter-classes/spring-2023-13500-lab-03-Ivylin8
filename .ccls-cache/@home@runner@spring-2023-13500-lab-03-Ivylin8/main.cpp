17913a32491d3c7945391c45656878417a51530b
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-02-16 06:03:21 +0000

    Task B

--- .ccls-cache/@home@runner@spring-2023-13500-lab-03-Ivylin8/main.cpp
+++ .ccls-cache/@home@runner@spring-2023-13500-lab-03-Ivylin8/main.cpp
@@ -10,7 +10,13 @@ int main()
 {
   std::cout<< "East Basin Storage: " << get_east_storage("01/01/2018") << "billion gallons"<<std::endl;
    std::cout<< "East Basin Storage: " << get_east_storage("12/01/2018") << "billion gallons"<<std::endl;
-  
+
+//Task b
+  std::cout << "\nminiumn storage in East basin in 2018: " << get_min_east() << "billion gallons" <<std::endl;
+  std::cout << "\nmaxium storage in East basin in 2018: " << get_max_east() << "billion gallons" <<std::endl;
+
+
+
   
 
 return 0;
--- .ccls-cache/@home@runner@spring-2023-13500-lab-03-Ivylin8/reservoir.cpp
+++ .ccls-cache/@home@runner@spring-2023-13500-lab-03-Ivylin8/reservoir.cpp
@@ -31,4 +31,56 @@ double get_east_storage(std::string date)
 	return 0;
 }
 
+//TASK B
+ double get_min_east()
+{
+  std::ifstream dataFile("Current_Reservoir_Levels.tsv");
+  if (dataFile.fail()){
+    std::cerr <<"File can not be opened for reading." <<std::endl;
+    exit(1);
+  }
+  //Ignore first header
+  std::string junk;
+  std::getline(dataFile, junk);
+  //reading files
+  std::string entry_date, east_basin_storage;
+  double min_storage = INT_MAX;
+  while(dataFile >> entry_date >> east_basin_storage) {
+    if (date == entry_date) {
+      min_storage = std::min(min_storage, std::stold(east_basin_storage));
+    }
+
+    dataFile.ignore(INT_MAX, '\n');
+  }
+
+  dataFile.close(); 
+  return min_storage;
+}
+
+double get_max_east()
+{
+  std::ifstream dataFile("Current_Reservoir_Levels.tsv");
+  if (dataFile.fail()){
+    std::cerr <<"File can not be opened for reading." <<std::endl;
+    exit(1);
+  }
+  //Ignore first header
+  std::string junk;
+  std::getline(dataFile, junk);
+  //reading files
+  std::string entry_date, east_basin_storage;
+  double max_storage = -1;
+  while(dataFile >> entry_date >> east_basin_storage) {
+    if (date == entry_date) {
+      max_storage = std::max(max_storage, std::stold(east_basin_storage));
+    }
+
+    dataFile.ignore(INT_MAX, '\n');
+  }
+
+  dataFile.close(); 
+  return max_storage;
+}
+ 
+
  
